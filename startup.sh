03106b7114d8994b30fe3d499b21401d6603cd56
Matheus Araujo <matheus.araujo.r42@gmail.com>
2020-07-05 00:05:37 -0300

    add redshift

--- startup.sh
+++ startup.sh
@@ -109,6 +109,9 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
+echo 'installing redshift'
+sudo apt install redshift
+
 echo 'installing postman'
 sudo snap install postman
 
